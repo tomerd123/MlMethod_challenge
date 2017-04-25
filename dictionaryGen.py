25f6a9fb5c9b02237444c2cf8b336891539e9076
tomerd123 <tomerdoi@post.bgu.ac.il>
2017-04-25 03:00:44 +0300

    first push

--- dictionaryGen.py
+++ dictionaryGen.py
@@ -36,13 +36,53 @@ def createNGram2Dic ():
                 else:
                     ngram2Dic[lines[row][:-1] + "_" + lines[row+1][:-1]] += 1
                 counter += 1
+def createNGram3Dic ():
+    for i in range(10):
+        with io.open('E:/challenge/training_data/User' + str(i), 'rt', encoding="utf8") as file:
+
+            lines=file.readlines()
+            counter = 1
+
+            for row in range(len(lines)):
+                if (counter+1)%100==0 or counter%100==0:
+                    counter+=1
+                    continue
+                if ngram3Dic.__contains__(lines[row][:-1] + "_" + lines[row+1][:-1]+ "_" + lines[row+2][:-1]) == False:
+                    ngram3Dic[lines[row][:-1] + "_" + lines[row+1][:-1]+ "_" + lines[row+2][:-1]] = 1
+                else:
+                    ngram3Dic[lines[row][:-1] + "_" + lines[row+1][:-1]+ "_" + lines[row+2][:-1]] += 1
+                counter += 1
+
+def createNGram4Dic ():
+    for i in range(10):
+        with io.open('E:/challenge/training_data/User' + str(i), 'rt', encoding="utf8") as file:
+
+            lines=file.readlines()
+            counter = 1
+
+            for row in range(len(lines)):
+                if (counter+2)%100==0 or (counter+1)%100==0 or counter%100==0:
+                    counter+=1
+                    continue
+                if ngram4Dic.__contains__(lines[row][:-1] + "_" + lines[row+1][:-1]+ "_" + lines[row+2][:-1]+ "_" + lines[row+3][:-1]) == False:
+                    ngram4Dic[lines[row][:-1] + "_" + lines[row+1][:-1]+ "_" + lines[row+2][:-1]+ "_" + lines[row+3][:-1]] = 1
+                else:
+                    ngram4Dic[lines[row][:-1] + "_" + lines[row+1][:-1]+ "_" + lines[row+2][:-1]+ "_" + lines[row+3][:-1]] += 1
+                counter += 1
 
 
 commandDic={}
 ngram2Dic={}
+ngram3Dic={}
+ngram4Dic={}
+
 createCommandDic()
 createNGram2Dic()
+createNGram3Dic()
+createNGram4Dic()
 
-for x in ngram2Dic:
-    print(str(x)+"_"+str(ngram2Dic[x]))
+for x in ngram4Dic:
+    print(str(x)+"_"+str(ngram4Dic[x]))
+
+
 print("finished")
